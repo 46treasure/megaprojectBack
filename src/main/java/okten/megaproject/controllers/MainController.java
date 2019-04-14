2f69c2c9145cf267493f7db78a2e51aaad13414c
NotProfessional <igor.karpyuk00@gmail.com>
2019-04-14 14:28:10 +0300

    add img config

--- src/main/java/okten/megaproject/controllers/MainController.java
+++ src/main/java/okten/megaproject/controllers/MainController.java
@@ -4,6 +4,7 @@ import okten.megaproject.Service.FilmService;
 import okten.megaproject.dao.FilmsDao;
 import okten.megaproject.models.Films;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -34,10 +35,14 @@ public class MainController {
                          @RequestParam("country") String country,
                          @RequestParam("aboutFilm") String aboutFilm,
                          @RequestParam("quality") String quality ,
+                         BindingResult bindingResult,
                          @RequestParam("picture") MultipartFile picture){
-        Films film = new Films(name,year,aboutFilm,country,quality);
+        if(bindingResult.hasErrors()) {
+            return null;
+        }
+        Films film = new Films(name, year, aboutFilm, country, quality);
         filmService.transferTo(picture);
-        film.setPicture(picture.getOriginalFilename());
+        film.setPicture(path + picture.getOriginalFilename());
         return filmsDao.save(film);
     }
 
