6f0e1c81629f4ca003d2d67c2c39ed86430e6bdb
46treasure <45ytrasure@gmail.com>
2019-06-03 16:12:51 +0300

    some

--- src/main/java/okten/megaproject/dao/FilmsDao.java
+++ src/main/java/okten/megaproject/dao/FilmsDao.java
@@ -2,11 +2,12 @@ package okten.megaproject.dao;
 
 import okten.megaproject.models.Films;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import java.util.List;
 
 public interface FilmsDao extends JpaRepository<Films, Long> {
     Films findFilmsByName(String name);
+
 
     @Override
     Films getOne(Long aLong);
