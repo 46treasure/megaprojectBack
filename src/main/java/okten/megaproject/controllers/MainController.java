426a727418e31f2ad52567fe48c68733b23808b0
NotProfessional <igor.karpyuk00@gmail.com>
2019-07-01 09:28:04 +0300

    update current

--- src/main/java/okten/megaproject/controllers/MainController.java
+++ src/main/java/okten/megaproject/controllers/MainController.java
@@ -1,41 +1,32 @@
 package okten.megaproject.controllers;
-
-import okten.megaproject.Configurations.EmailService;
 import okten.megaproject.Service.FilmService;
-import okten.megaproject.Service.UserService;
-import okten.megaproject.dao.CommentDAO;
 import okten.megaproject.dao.FilmsDao;
 import okten.megaproject.dao.UserDao;
 import okten.megaproject.models.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-
 import java.util.ArrayList;
-import java.util.Iterator;
+import java.util.Collections;
 import java.util.List;
 
 @RestController
 public class MainController {
-    String path = "http://127.0.0.1:8887/";
+    private String path = "http://127.0.0.1:8887/";
+    private final FilmsDao filmsDao;
+    private final FilmService filmService;
+    private final UserDao userDao;
 
     @Autowired
-    FilmsDao filmsDao;
+    public MainController(FilmsDao filmsDao, FilmService filmService, UserDao userDao) {
+        this.filmsDao = filmsDao;
+        this.filmService = filmService;
+        this.userDao = userDao;
+    }
+
     @Autowired
-    FilmService filmService;
-    @Autowired
-    UserDao userDao;
-    @Autowired
-    UserService userService;
-    @Autowired
-    EmailService emailService;
-    @Autowired
-    CommentDAO commentDAO;
     @GetMapping("/home")
     public List<Films> allFilms() {
-        System.out.println(SecurityContextHolder.getContext().getAuthentication().getDetails());
         return filmsDao.findAll();
 
     }
@@ -79,8 +70,7 @@ public class MainController {
                          @RequestParam("imgActor4") MultipartFile imgActor4
                          ) {
         ArrayList<String> genres = new ArrayList<>();
-        for (String rev : genre.split(","))
-            genres.add(rev);
+        Collections.addAll(genres, genre.split(","));
         Films film = new Films(name, year, aboutFilm, country, quality);
         ArrayList<Integer> listRating = new ArrayList<>();
         film.setRating(listRating);
@@ -98,7 +88,7 @@ public class MainController {
         return filmsDao.save(film);
     }
 
-    ArrayList<String> createList(MultipartFile file1,MultipartFile file2,MultipartFile file3,MultipartFile file4) {
+    private ArrayList<String> createList(MultipartFile file1,MultipartFile file2,MultipartFile file3,MultipartFile file4) {
         ArrayList<String> list = new ArrayList<>();
         transferFour(file1,file2,file3,file4);
         list.add(path + file1.getOriginalFilename());
@@ -107,7 +97,7 @@ public class MainController {
         list.add(path + file4.getOriginalFilename());
         return list;
     }
-         ArrayList<Actors> addActors (String actor1, MultipartFile imgActor1,
+        private ArrayList<Actors> addActors (String actor1, MultipartFile imgActor1,
                          String actor2, MultipartFile imgActor2,
                          String actor3, MultipartFile imgActor3,
                          String actor4, MultipartFile imgActor4) {
@@ -120,14 +110,13 @@ public class MainController {
         return actors;
          }
 
-         void transferFour(MultipartFile file1,MultipartFile file2,MultipartFile file3,MultipartFile file4) {
+         private void transferFour( MultipartFile file1,MultipartFile file2,MultipartFile file3,MultipartFile file4) {
              filmService.transferTo(file1);
              filmService.transferTo(file2);
              filmService.transferTo(file3);
              filmService.transferTo(file4);
          }
     @PostMapping("/getbyid")
-    @CrossOrigin(origins = "http://localhost:4200")
     public Films getById(@RequestBody Long id) {
         return filmsDao.getOne(id);
     }
@@ -137,96 +126,11 @@ public class MainController {
         return filmService.findByGenre(genre);
     }
 
-
     @PostMapping("/delfilm")
     public List<Films> delFilm(@RequestBody Long filmId) {
         filmsDao.deleteById(filmId);
         return filmsDao.findAll();
     }
-
-
-    @Autowired
-    PasswordEncoder passwordEncoder;
-
-    @PostMapping("/reg")
-    public boolean reg(@RequestBody User user) {
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
-        User userDb = userDao.findByUsername(user.getUsername());
-        if (userDb != null) {
-            return false;
-        } else {
-            user.setAvatar("assets/ava.jpg");
-            user.setRole(UserEnum.ROLE_USER);
-            ArrayList<Integer> sub = new ArrayList<>();
-            ArrayList<Integer> folow = new ArrayList<>();
-            user.setFolowing(folow);
-            user.setSubscribes(sub);
-            userDao.save(user);
-            emailService.send(user.getEmail(), user);
-            return true;
-        }
-    }
-
-    private User current = new User();
-
-    @GetMapping("/get")
-    public User get() {
-        String authentication = SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString();
-        current = userDao.findByUsername(authentication);
-        return current;
-    }
-
-    @PostMapping("/setStatus")
-    public void setStatus(@RequestBody String status) {
-        current = get();
-        current.setStatus(status);
-        userDao.save(current);
-    }
-
-    @PostMapping("/adduserfilm")
-    public List<Films> addUserFilm(@RequestBody Long idFilm) {
-        String auth = SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString();
-        User byUsername = userDao.findByUsername(auth);
-        List<Films> usersFilms = byUsername.getUsersFilms();
-        for (int i = 0; i < usersFilms.size(); i++) {
-            if (usersFilms.get(i).getId() == idFilm) {
-                return usersFilms;
-            }
-        }
-        Films one = filmsDao.getOne(idFilm);
-        usersFilms.add(one);
-        List<User> users = one.getUser();
-        one.setUser(users);
-        byUsername.setUsersFilms(usersFilms);
-        userDao.save(byUsername);
-        filmsDao.save(one);
-        return usersFilms;
-    }
-
-    @PostMapping("/deluserfilms")
-    public List<Films> delUserFilm(@RequestBody long idFilm) {
-        current = get();
-        List<Films> usersFilms = current.getUsersFilms();
-        Iterator<Films> iterator = usersFilms.iterator();
-        while (iterator.hasNext()) {
-            Films next = iterator.next();
-            if (next.getId() == idFilm) {
-                iterator.remove();
-                break;
-            }
-        }
-        current.setUsersFilms(usersFilms);
-        userDao.save(current);
-        return usersFilms;
-    }
-
-    @PostMapping("/userpage-userfilms")
-    public List<Films> getUserFilm(@RequestBody int id) {
-        User byUsername = userDao.getOne(id);
-        List<Films> usersFilms = byUsername.getUsersFilms();
-        return usersFilms;
-    }
-
     @PostMapping("/rating")
     public double rating(@RequestParam("idFilm") Long id, @RequestParam("rating") int rat) {
         int sum = 0;
@@ -242,104 +146,7 @@ public class MainController {
         one.setScore(res);
         filmsDao.save(one);
         return res;
-        //ss
     }
-
-
-    private User thisUser = new User();
-
-    @PostMapping("/getUserById")
-    public User getUserById(@RequestBody int id) {
-        thisUser = userDao.getOne(id);
-        return thisUser;
-    }
-
-    @PostMapping("/currentPage")
-    public boolean currentPage(@RequestBody int id) {
-        if (current != null) {
-            current = get();
-            return current.getId() == id;
-        }
-        return false;
-    }
-
-    @PostMapping("/subscribe")
-    public void subscribe(@RequestBody int id) {
-        current = get();
-        User byId = userDao.getOne(id);
-        ArrayList<Integer> subscribes = byId.getSubscribes();
-        subscribes.add(current.getId());
-        byId.setSubscribes(subscribes);
-        userDao.save(byId);
-        ArrayList<Integer> folowing = current.getFolowing();
-        folowing.add(id);
-        userDao.save(current);
-        System.out.println(byId.getSubscribes());
-    }
-
-    @PostMapping("/unSubscribe")
-    public void unSubscribe(@RequestBody int id) {
-        current = get();
-        User byId = userDao.getOne(id);
-        ArrayList<Integer> subscribes = byId.getSubscribes();
-        Integer currentID = current.getId();
-        if (subscribes.contains(currentID)) {
-            subscribes.remove(currentID);
-            byId.setSubscribes(subscribes);
-            userDao.save(byId);
-        }
-        ArrayList<Integer> folowing = current.getFolowing();
-        Integer i = id;
-        if (folowing.contains(i)) {
-            folowing.remove(i);
-            current.setFolowing(folowing);
-            userDao.save(current);
-        }
-
-    }
-
-    @PostMapping("/getSubscribers")
-    public ArrayList<User> friends(@RequestBody int id) {
-        User one = userDao.getOne(id);
-        ArrayList<Integer> subscribes = one.getSubscribes();
-        ArrayList<User> friends = new ArrayList<>();
-        for (Integer subscribe : subscribes) {
-            friends.add(userDao.getOne(subscribe));
-        }
-
-        System.out.println(subscribes);
-        return friends;
-    }
-
-    @PostMapping("/exist")
-    public boolean exist(@RequestBody int id) {
-        if (get() != null) {
-            current = get();
-            ArrayList<Integer> following = current.getFolowing();
-            return following.contains(id);
-        }
-        return false;
-
-    }
-
-    @PostMapping("/getFolowing")
-    public ArrayList<User> folowing(@RequestBody int id) {
-        User one = userDao.getOne(id);
-        ArrayList<Integer> folowing = one.getFolowing();
-        ArrayList<User> folow = new ArrayList<>();
-        for (Integer folower : folowing) {
-            folow.add(userDao.getOne(folower));
-        }
-        return folow;
-    }
-
-    @PostMapping("/setAvatar")
-    public void setAva(@RequestParam("avatar") MultipartFile avatar) {
-        current = get();
-        userService.saveAva(avatar);
-        userDao.setAvatar(path + avatar.getOriginalFilename(), current.getId());
-    }
-
     @PostMapping("/close")
     public void close(@RequestBody int id) {
         System.out.println("!!" + id);
@@ -349,51 +156,10 @@ public class MainController {
             userDao.save(onUser);
         }
     }
-
-
-    @PostMapping("/search")
-    public List<Films> search(@RequestBody String name) {
-        return filmService.searchFilms(name);
-    }
-
-    @GetMapping("/finishReg/{key}")
-    public boolean finish(@PathVariable String key) {
-        System.out.println(key);
-        return userService.activateUser(key);
-
-    }
-    @GetMapping("/getAllUsers")
-    public List<User> getAllUsers() {
-        return userDao.findAll();
-    }
-
-    @PostMapping("/findSearchingUser")
-    public List<User> findSearchingFilm(@RequestBody String name) {
-        return userService.searchUser(name);
-    }
-
-
-    @PostMapping("/addComment")
-    public void addComent( @RequestParam String idfilm,
-                           @RequestParam String text,
-                           @RequestParam String date) {
-
-        Films currentFilm = filmsDao.getOne(Long.parseLong(idfilm));
-        current = get();
-        Comments comments = new Comments();
-        comments.setUserID(current.getId());
-        comments.setUsername(current.getUsername());
-        comments.setAvatar(current.getAvatar());
-        comments.setText(text);
-        comments.setDate(date);
-        comments.setFilm(currentFilm);
-        commentDAO.save(comments);
-    }
     @PostMapping("/getComments")
     public List<Comments> getComments (@RequestBody Long id){
         Films current = filmsDao.getOne(id);
-        List<Comments> comments = current.getComments();
-        return comments;
+        return current.getComments();
 
     }
 }
